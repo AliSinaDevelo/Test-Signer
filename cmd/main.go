c7532b5e2945a8f9e4c9136fe45e512ca2a9fb97
AliSina Karimi <100498538+AliSinaDevelo@users.noreply.github.com>
2023-12-11 15:01:33 +0100

    database connection and server initialization

---
+++ cmd/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+	"internal/database/postgres"
+	"internal/handler"
+)
+
+func main() {
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	// initialize database connection
+	db, err := postgres.NewDB(dbHost, dbPort, dbUser, dbPassword, dbName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// initialize HTTP server
+	router := mux.NewRouter()
+
+	// Handlers for signing and verifying
+	signHandler := handler.NewSignHandler(db)
+	verifyHandler := handler.NewVerifyHandler(db)
+
+	router.HandleFunc("/sign", signHandler.Sign).Methods("POST")
+	router.HandleFunc("/verify", verifyHandler.Verify).Methods("POST")
+
+	port := ":8080"
+	fmt.Printf("Listening on port %s\n", port)
+	log.Fatal(http.ListenAndServe(port, router))
+
+	
+}	
