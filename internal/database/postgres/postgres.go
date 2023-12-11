fc117ec4710f07bc65ea4b83c3212033f12c84b6
AliSina Karimi <100498538+AliSinaDevelo@users.noreply.github.com>
2023-12-11 15:46:34 +0100

    NewDB creates a database connection

---
+++ internal/database/postgres/postgres.go
@@ -0,0 +1,22 @@
+package postgres
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+func NewDB(host, port, user, password, dbName string) *sql.DB {
+	dbinfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbName)
+	db, err := sql.Open("postgres", dbinfo)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	fmt.Println("Successfully connected!")
+	return db, nil
+}
