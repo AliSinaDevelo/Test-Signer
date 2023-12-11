2608381acc0a1c83314ce914759958a27142fb8e
AliSina Karimi <100498538+AliSinaDevelo@users.noreply.github.com>
2023-12-11 15:31:15 +0100

    verify() handlew HTTP GET request for verifying the test signature

--- internal/handlers/verify_handler.go
+++ internal/handlers/verify_handler.go
@@ -14,3 +14,26 @@ type VerifyHandler struct {
 func NewVerifyHandler(db *sql.DB) *VerifyHandler {
 	return &VerifyHandler{DB: db}
 }
+
+func (vh *VerifyHandler) Verify(w http.ResponseWriter, r *http.Request) {
+	user := r.URL.Query().Get("user")
+	signature := r.URL.Query().Get("signature")
+
+	var fetchedSig Signature
+	row := vh.DB.QueryRow("SELECT signature FROM signatures WHERE username = $1 AND signaturee = $2 ", user, signature)
+	err := row.Scan(&fetchedSignature.User, &fetchedSignature.Signature, &fetchedSignature.Answers, &fetchedSignature.Timestamp)
+	if err != nil {
+		http.Error(w, "Invalid signature opr user", http.StatusBadRequest)
+		return
+	}
+
+
+	// return the signature
+	response := map[string]interface{}{
+		"status": "success",
+		"user": fetchedSignature.User,
+		"answers": fetchedSignature.Answers,
+		"timestamp": fetchedSignature.Timestamp.Format(time.RFC3339),
+	}
+	json.NewEncoder(w).Encode(response)
+}
