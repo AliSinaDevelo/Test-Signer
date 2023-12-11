0345ae265bdd49ea306319aa69f2be6e8e6e69a7
AliSina Karimi <100498538+AliSinaDevelo@users.noreply.github.com>
2023-12-11 15:14:24 +0100

    signHandler struct and NewSignHandler function

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module AliSinaDevelo
 go 1.18
 
 require (
-	github.com/gorilla/mux v1.8.1 // indirect
-	github.com/lib/pq v1.10.9 // indirect
+	github.com/gorilla/mux v1.8.1
+	github.com/lib/pq v1.10.9
 )
+
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
---
+++ internal/handlers/sign_handler.go
@@ -0,0 +1,58 @@
+package handlers
+
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type SignHandler struct {
+	db *sql.DB
+}
+
+func NewSignHandler(db *sql.DB) *SignHandler {
+	return &SignHandler{DB: db}
+}
+
+func (h *SignHandler) Sign(w http.ResponseWriter, r *http.Request) {
+	var requestData struct {
+		Username string `json:"username"`
+		Answer  []QuestionAnswer `json:"answer"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&requestData)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// generate jwt token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username": requestData.Username,
+		"answer": requestData.Answer,
+		"exp": time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	tokenString, err := token.SignedString([]byte("secret"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	// save token to database
+
+	_, err = sh.DB.Exec("INSERT INTO sig (user_id, signature, answer, timestamp) VALUES ($1, $2, $3, $4)", requestData.Username, tokenString, requestData.Answer, time.Now())
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	response := map[string]string{
+		"token": tokenString,
+	}
+	json.NewEncoder(w).Encode(response)
+}
