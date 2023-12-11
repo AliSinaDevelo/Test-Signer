a5cbe8c7de1e52a1de44e6da359f9bc79b7a0e5f
AliSina Karimi <100498538+AliSinaDevelo@users.noreply.github.com>
2023-12-11 15:56:11 +0100

    LoadConfig() laods the configuration

--- config/config.go
+++ config/config.go
@@ -11,3 +11,13 @@ type AppConfig struct {
 	DBPassword string
 	DBName     string
 }
+
+func LoadConfig() *AppConfig {
+	return &AppConfig{
+		DBHost:     os.Getenv("DB_HOST"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		DBName:     os.Getenv("DB_NAME"),
+	}
+}
